ddd88791882731c92a68cfd3207a9d34fd460e4a
Nate Beatty <nate.beatty@gmail.com>
2013-04-28 13:31:30 -0400

    Weekend brunch listing hack

--- js/controllers.js
+++ js/controllers.js
@@ -52,23 +52,45 @@ function currentMeal() {
 
 function parseResponse(data) {
   var jsonRaw = x2js.xml_str2json( data );
-  var jsonClean = {
-    'ross': {
-      'breakfast': (jsonRaw.root.menu[0].Breakfast) ? jsonRaw.root.menu[0].Breakfast.item_asArray : [],
-      'lunch': (jsonRaw.root.menu[0].Lunch) ? jsonRaw.root.menu[0].Lunch.item_asArray : [],
-      'dinner': (jsonRaw.root.menu[0].Dinner) ? jsonRaw.root.menu[0].Dinner.item_asArray : []
-    },
-    'proctor': {
-      'breakfast': (jsonRaw.root.menu[1].Breakfast) ? jsonRaw.root.menu[1].Breakfast.item_asArray : [],
-      'lunch': (jsonRaw.root.menu[1].Lunch) ? jsonRaw.root.menu[1].Lunch.item_asArray : [],
-      'dinner': (jsonRaw.root.menu[1].Dinner) ? jsonRaw.root.menu[1].Dinner.item_asArray : [],
-    },
-    'atwater': {
-      'breakfast': (jsonRaw.root.menu[2].Breakfast) ? jsonRaw.root.menu[2].Breakfast.item_asArray : [],
-      'lunch': (jsonRaw.root.menu[2].Lunch) ? jsonRaw.root.menu[2].Lunch.item_asArray : [],
-      'dinner': (jsonRaw.root.menu[2].Dinner) ? jsonRaw.root.menu[2].Dinner.item_asArray : []
-    }
-  };
+  var jsonClean;
+
+  if (d.getDay() == 0 || d.getDay() == 6) {
+    jsonClean = {
+      'ross': {
+        'breakfast': (jsonRaw.root.menu[0].Breakfast) ? jsonRaw.root.menu[0].Breakfast.item_asArray : [],
+        'lunch': (jsonRaw.root.menu[0].Lunch) ? jsonRaw.root.menu[0].Breakfast.item_asArray : [],
+        'dinner': (jsonRaw.root.menu[0].Dinner) ? jsonRaw.root.menu[0].Dinner.item_asArray : []
+      },
+      'proctor': {
+        'breakfast': (jsonRaw.root.menu[1].Breakfast) ? jsonRaw.root.menu[1].Lunch.item_asArray : [],
+        'lunch': (jsonRaw.root.menu[1].Lunch) ? jsonRaw.root.menu[1].Lunch.item_asArray : [],
+        'dinner': (jsonRaw.root.menu[1].Dinner) ? jsonRaw.root.menu[1].Dinner.item_asArray : [],
+      },
+      'atwater': {
+        'breakfast': (jsonRaw.root.menu[2].Breakfast) ? jsonRaw.root.menu[2].Breakfast.item_asArray : [],
+        'lunch': (jsonRaw.root.menu[2].Lunch) ? jsonRaw.root.menu[2].Lunch.item_asArray : [],
+        'dinner': (jsonRaw.root.menu[2].Dinner) ? jsonRaw.root.menu[2].Dinner.item_asArray : []
+      }
+    };
+  } else {
+    jsonClean = {
+      'ross': {
+        'breakfast': (jsonRaw.root.menu[0].Breakfast) ? jsonRaw.root.menu[0].Breakfast.item_asArray : [],
+        'lunch': (jsonRaw.root.menu[0].Lunch) ? jsonRaw.root.menu[0].Lunch.item_asArray : [],
+        'dinner': (jsonRaw.root.menu[0].Dinner) ? jsonRaw.root.menu[0].Dinner.item_asArray : []
+      },
+      'proctor': {
+        'breakfast': (jsonRaw.root.menu[1].Breakfast) ? jsonRaw.root.menu[1].Breakfast.item_asArray : [],
+        'lunch': (jsonRaw.root.menu[1].Lunch) ? jsonRaw.root.menu[1].Lunch.item_asArray : [],
+        'dinner': (jsonRaw.root.menu[1].Dinner) ? jsonRaw.root.menu[1].Dinner.item_asArray : [],
+      },
+      'atwater': {
+        'breakfast': (jsonRaw.root.menu[2].Breakfast) ? jsonRaw.root.menu[2].Breakfast.item_asArray : [],
+        'lunch': (jsonRaw.root.menu[2].Lunch) ? jsonRaw.root.menu[2].Lunch.item_asArray : [],
+        'dinner': (jsonRaw.root.menu[2].Dinner) ? jsonRaw.root.menu[2].Dinner.item_asArray : []
+      }
+    };
+  }
 
   return jsonClean;
 }
