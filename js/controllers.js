982f5fd1c314501e50627e0dd01a5492fa78083d
Nate Beatty <nate.beatty@gmail.com>
2013-04-28 12:52:01 -0400

    Mobile user message

--- js/controllers.js
+++ js/controllers.js
@@ -31,6 +31,8 @@ function MenuDataCtrl($scope, $http) {
 
   $scope.meal = currentMeal();
 
+  $scope.isMobile = (detectMobile()) ? true : false;
+
 }
 
 function currentMeal() {
@@ -68,7 +70,21 @@ function parseResponse(data) {
     }
   };
 
-//  console.log("Some Items: " + jsonClean.ross.breakfast);
-
   return jsonClean;
+}
+
+function detectMobile() { 
+ if( navigator.userAgent.match(/Android/i)
+   || navigator.userAgent.match(/webOS/i)
+   || navigator.userAgent.match(/iPhone/i)
+   || navigator.userAgent.match(/iPad/i)
+   || navigator.userAgent.match(/iPod/i)
+   || navigator.userAgent.match(/BlackBerry/i)
+   || navigator.userAgent.match(/Windows Phone/i)
+   ){
+  return true;
+}
+else {
+  return false;
+}
 }
