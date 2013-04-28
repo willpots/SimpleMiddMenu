b4435331a2eec300e51496e9188d178417e30817
Nate Beatty <nate.beatty@gmail.com>
2013-04-28 09:36:59 -0400

    Slightly Improved Nav

--- js/controllers.js
+++ js/controllers.js
@@ -6,6 +6,8 @@ var d = new Date();
 var today = d.getFullYear().toString() + '-' + (d.getMonth() + 1).toString() + '-' + d.getDate().toString();
 var apiURL = 'http://menus.middlebury.edu/' + today + '/xml';
 
+var apiURL = 'http://menus.middlebury.edu/' + today + '/xml';
+
 function MenuDataCtrl($scope, $http) {
 
   $http({
@@ -16,8 +18,7 @@ function MenuDataCtrl($scope, $http) {
       'Accept': 'text/xml'
     },
     transformResponse: function(data) {
-      var json = x2js.xml_str2json( data );
-      return json;
+      return parseResponse( data );
     },
     cache: false,
   }).
@@ -27,4 +28,30 @@ function MenuDataCtrl($scope, $http) {
   error(function(data, status) {
     $scope.menuData = data || "Request failed";
   });
+
+}
+
+function parseResponse(data) {
+  var jsonRaw = x2js.xml_str2json( data );
+  var jsonClean = {
+    'ross': {
+      'breakfast': (jsonRaw.root.menu[0].Breakfast) ? jsonRaw.root.menu[0].Breakfast.item_asArray : [],
+      'lunch': (jsonRaw.root.menu[0].Lunch) ? jsonRaw.root.menu[0].Lunch.item_asArray : [],
+      'dinner': (jsonRaw.root.menu[0].Dinner) ? jsonRaw.root.menu[0].Dinner.item_asArray : []
+    },
+    'proctor': {
+      'breakfast': (jsonRaw.root.menu[1].Breakfast) ? jsonRaw.root.menu[1].Breakfast.item_asArray : [],
+      'lunch': (jsonRaw.root.menu[1].Lunch) ? jsonRaw.root.menu[1].Lunch.item_asArray : [],
+      'dinner': (jsonRaw.root.menu[1].Dinner) ? jsonRaw.root.menu[1].Dinner.item_asArray : [],
+    },
+    'atwater': {
+      'breakfast': (jsonRaw.root.menu[2].Breakfast) ? jsonRaw.root.menu[2].Breakfast.item_asArray : [],
+      'lunch': (jsonRaw.root.menu[2].Lunch) ? jsonRaw.root.menu[2].Lunch.item_asArray : [],
+      'dinner': (jsonRaw.root.menu[2].Dinner) ? jsonRaw.root.menu[2].Dinner.item_asArray : []
+    }
+  };
+
+  console.log("Some Items: " + jsonClean.ross.breakfast);
+
+  return jsonRaw;
 }
